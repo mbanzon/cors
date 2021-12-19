4826cd780d5f5025c1694a0da293d2b39463a5e0
Michael Banzon <michael@banzon.dk>
2021-12-19 17:22:42 +0100

    Update cors.go
    Fixing code so string joins + conversion if done when configuring instead of on every request.

--- cors.go
+++ cors.go
@@ -10,10 +10,10 @@ import (
 // Cors holds the functions and data configured and provide the middleware
 // used for CORS (Cross-origin resource sharing).
 type Cors struct {
-	allowedOrigins []string
-	allowedHeaders []string
-	allowedMethods []string
-	maxAge         time.Duration
+	allowedOrigins string
+	allowedHeaders string
+	allowedMethods string
+	maxAge         string
 }
 
 // ConfigFunc is the type of function used to configure the Cors
@@ -35,19 +35,19 @@ func New(configs ...ConfigFunc) *Cors {
 
 func (c *Cors) Wrap(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if len(c.allowedOrigins) > 0 {
-			w.Header().Add("Access-Control-Allow-Origin", strings.Join(c.allowedOrigins, ", "))
+		if c.allowedOrigins != "" {
+			w.Header().Add("Access-Control-Allow-Origin", c.allowedOrigins)
 		}
-		if len(c.allowedMethods) > 0 {
-			w.Header().Add("Access-Control-Allow-Methods", strings.Join(c.allowedMethods, ", "))
+		if c.allowedMethods != "" {
+			w.Header().Add("Access-Control-Allow-Methods", c.allowedMethods)
 		}
-		if len(c.allowedHeaders) > 0 {
-			w.Header().Add("Access-Control-Allow-Headers", strings.Join(c.allowedHeaders, ", "))
+		if c.allowedHeaders != "" {
+			w.Header().Add("Access-Control-Allow-Headers", c.allowedHeaders)
 		}
 
 		if r != nil && r.Method == http.MethodOptions {
-			if c.maxAge > 0 {
-				w.Header().Add("Access-Control-Max-Age", fmt.Sprint(int(c.maxAge.Seconds())))
+			if c.maxAge != "" {
+				w.Header().Add("Access-Control-Max-Age", c.maxAge)
 			}
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -61,7 +61,7 @@ func (c *Cors) Wrap(h http.Handler) http.Handler {
 // header that signals that only requests from the given hosts are accepted.
 func WithOrigins(origins ...string) ConfigFunc {
 	return func(c *Cors) {
-		c.allowedOrigins = origins
+		c.allowedOrigins = strings.Join(origins, ", ")
 	}
 }
 
@@ -70,7 +70,7 @@ func WithOrigins(origins ...string) ConfigFunc {
 // are accepted.
 func WithMethods(methods ...string) ConfigFunc {
 	return func(c *Cors) {
-		c.allowedMethods = methods
+		c.allowedMethods = strings.Join(methods, ", ")
 	}
 }
 
@@ -79,7 +79,7 @@ func WithMethods(methods ...string) ConfigFunc {
 // request method OPTIONS) could be cached for the given amount of time.
 func WithMaxAge(age time.Duration) ConfigFunc {
 	return func(c *Cors) {
-		c.maxAge = age
+		c.maxAge = fmt.Sprint(int(age.Seconds()))
 	}
 }
 
@@ -87,6 +87,6 @@ func WithMaxAge(age time.Duration) ConfigFunc {
 // a header that signals that only the given headers are accepted.
 func WithHeaders(headers ...string) ConfigFunc {
 	return func(c *Cors) {
-		c.allowedHeaders = headers
+		c.allowedHeaders = strings.Join(headers, ", ")
 	}
 }
